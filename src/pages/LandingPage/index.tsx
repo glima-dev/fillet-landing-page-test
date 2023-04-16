b80c3a93a192f1cb091863df667dbae4b0c19cc6
glima-dev <guilherme.lima.tec@live.com>
2023-04-16 18:53:00 -0300

    feat: create landing page structure

---
+++ src/pages/LandingPage/index.tsx
@@ -0,0 +1,23 @@
+import { StyledBackGround } from "./styles";
+import Footer from "../../components/Footer";
+
+const LandingPage = () => {
+  return (
+    <StyledBackGround>
+      {/*       <div>
+        <Header />
+        <TopSection />
+      </div>
+      <main>
+        <section>
+          <TopMainSectionContent />
+        </section>
+        <InfoCardsSection />
+        <BottonSection />
+      </main> */}
+      <Footer />
+    </StyledBackGround>
+  );
+};
+
+export default LandingPage;
---
+++ src/pages/LandingPage/styles.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const StyledBackGround = styled.div`
+  min-height: 100vh;
+  width: 100%;
+`;
